696f92bc5064864406a0df91c8cc90850758432f
obake-fe <daichismash@gmail.com>
2022-01-06 22:42:48 +0900

    refactor: 3-15

--- www/html/3-15/action.php
+++ www/html/3-15/action.php
@@ -1,15 +1,5 @@
 <?php declare(strict_types=1);
 //(2) action.php : postのaction先。テキストボックスの内容をCookieに保存する
 
-function escape($value): string {
-	return htmlspecialchars($value, ENT_QUOTES | ENT_HTML5);
-}
-if (!isset($_GET["subscribe"])) {
-	echo "Not Permitted";
-	return;
-}
-
-$textValue = escape($_GET["name"]);
-setcookie("name", $textValue, time() + 60 * 60, "/", "", false, true);
-
+setcookie("name", $_GET["name"], time() + 60 * 60, "/", "", false, true);
 header("Location:view.php", true, 307);
