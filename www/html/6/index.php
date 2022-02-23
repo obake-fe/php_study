c5ab32fa3be988570ab4a0d10a4077d4cb879d3a
obake-fe <daichismash@gmail.com>
2022-02-24 00:31:08 +0900

    chore: install laravel

--- www/html/6/index.php
+++ www/html/6/index.php
@@ -4,22 +4,26 @@
 <!doctype html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>6. Laravel導入</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>6. Laravel導入</title>
 </head>
 <body>
-	<h1>6. Laravel導入</h1>
-	<h2>1. mvcについて調べて説明してください<br>webの3層構造について調べてください</h2>
-	<h3>mvcとは</h3>
-	<p>プログラム処理をM（Model）、V（View）、C（Controller）という概念的な役割に分け、それぞれの役割が協調し合うことで1つの画面機能を実現するという、ソフトウェアアーキテクチャのこと。</p>
-	<ul>
-		<li>Model-ビジネスロジックを担当する</li>
-		<li>View-表示データを担当する</li>
-		<li>Model-全体のコントロールを担当する</li>
-	</ul>
-	<hr>
+<h1>6. Laravel導入</h1>
+<h2>1. mvcについて調べて説明してください<br>webの3層構造について調べてください</h2>
+<h3>mvcとは</h3>
+<p>プログラム処理をM（Model）、V（View）、C（Controller）という概念的な役割に分け、それぞれの役割が協調し合うことで1つの画面機能を実現するという、ソフトウェアアーキテクチャのこと。</p>
+<ul>
+    <li>Model-ビジネスロジックを担当する</li>
+    <li>View-表示データを担当する</li>
+    <li>Model-全体のコントロールを担当する</li>
+</ul>
+<hr>
+
+<h2>2. laravelをインストールしてhello worldを表示してください。<br>なお、php artisan serveは使わずに、composerなど、必要なものがあるので準備してください</h2>
+<p></p>
+<hr>
 
 </body>
 </html>
