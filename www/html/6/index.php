544bee2ea5d62555b8f1c62920ef26b03d639d4f
obake-fe <daichismash@gmail.com>
2022-02-23 21:17:34 +0900

    feat: add 6-1

---
+++ www/html/6/index.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+?>
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>6. Laravel導入</title>
+</head>
+<body>
+	<h1>6. Laravel導入</h1>
+	<h2>1. mvcについて調べて説明してください<br>webの3層構造について調べてください</h2>
+	<h3>mvcとは</h3>
+	<p>プログラム処理をM（Model）、V（View）、C（Controller）という概念的な役割に分け、それぞれの役割が協調し合うことで1つの画面機能を実現するという、ソフトウェアアーキテクチャのこと。</p>
+	<ul>
+		<li>Model-ビジネスロジックを担当する</li>
+		<li>View-表示データを担当する</li>
+		<li>Model-全体のコントロールを担当する</li>
+	</ul>
+	<hr>
+
+</body>
+</html>
--- www/html/index.php
+++ www/html/index.php
@@ -12,6 +12,7 @@
 	<h2><a href="3/index.php" target="_blank">3. PHP基礎</a></h2>
 	<h2><a href="4/index.php" target="_blank">4. SQL</a></h2>
 	<h2><a href="5/index.php" target="_blank">5. PHP応用</a></h2>
+	<h2><a href="6/index.php" target="_blank">6. Laravel導入</a></h2>
 	
 </body>
 </html>
